7d98fd68d40aeb1d5684dc587414177e445dac0d
Teresa Portone <tnportone@gmail.com>
2024-06-11 21:14:56 -0600

    Added cost model to the interactive simulation.

--- watershed_model.py
+++ watershed_model.py
@@ -40,12 +40,25 @@ class WatershedModel:
         if C is None: C = self.c
         return np.trapz(np.trapz(C,self.x,axis=1),self.y,axis=0)
     
-    def plot_concentration(self,C=None):
+    def plot_concentration(self,C=None, source_mitigation=None, sink_rate=None):
         if C is None: C = self.c
         fig = plt.figure(figsize=(4,3))
         ax = fig.add_subplot(111)
         ax.imshow(C, vmin=0, vmax=self.c_nm.max(), cmap='Greys', origin='lower')
         ax.annotate(f"{100*(1-self.get_total_contaminant(C)):.0f}% removed", (0.02,0.97), xycoords='axes fraction', va='top')
+
+        if not source_mitigation is None:
+            sm_cost = source_mitigation*1e7
+            sr_cost = sink_rate*5e6
+            total_cost = sm_cost + sr_cost
+            budget=3e6
+            ax.annotate(f"Budget: ${budget:,.0f}", (0.02, 0.87), xycoords='axes fraction', va='top')
+            ax.annotate("Total cost:", (0.02, 0.78), xycoords='axes fraction', va='top')
+            if total_cost <= budget:
+                ax.annotate(f"${total_cost:,.0f}", (0.22, 0.78), xycoords='axes fraction', va='top')
+            else:
+                ax.annotate(f"${total_cost:,.0f}", (0.22, 0.78), xycoords='axes fraction', va='top', color='red')
+
         ax.set_xticks([])
         ax.set_yticks([])
         fig.tight_layout()
@@ -55,23 +68,24 @@ class WatershedModel:
                   sink_rate=FloatSlider(description="Soil treatment", min=0, max=1, step=0.1, value=0.0, style=dict(description_width='initial')))
         def plot_animation(source_mitigation, sink_rate):
             C = self.run_simulation(1, source_mitigation, sink_rate)
-            self.plot_concentration(C)
+            self.plot_concentration(C, source_mitigation, sink_rate)
 
     def simulation_movie(self):
         fig, ax = plt.subplots(figsize=(4, 3))
 
+        ax.set_title("Contamination Spreading in a Watershed")
         C = self.run_simulation(1)
         im = ax.imshow(self.IC, cmap='Greys')#, vmin=0, vmax=self.IC.max(), cmap='Greys', origin='lower')
         ax.set_xticks([])
         ax.set_yticks([])
-        ax.set_title('Time: 0')
+        time_text = ax.annotate('0 days', (0.02, 0.97), xycoords='axes fraction', va='top')
         fig.tight_layout()
 
         def update(t):
             C = self.run_simulation(t)
             im.set_array(C)
             im.set_clim(vmin=0, vmax=np.max(C))
-            ax.set_title(f'{365*t:.0f} days')
+            time_text.set_text(f'{365*t:.0f} days')
             return im,
 
         ani = FuncAnimation(fig, update, frames=np.linspace(0, 1, 100), interval=80, blit=True)
